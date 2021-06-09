15ec6571c9cf52606a796f9ad67fd2f77184e8cf
Crisleop21 <leonelporoj@gmail.com>
2021-06-08 23:53:17 -0600

    Ejercicio5 Primer commit

---
+++ Ejercicio5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Ejercicio5{
+	public static void main(String[] args) {
+		//inicio del programa, declaramos variables e inicializamos scanner
+		Scanner scanner = new Scanner(System.in);
+		double a;
+		double nextNum;
+		//Solicitamos que igrese los valores y los leemos	
+		System.out.println("Ingrese un numero: ");
+		a = scanner.nextInt();
+		//contamos los numeros
+		nextNum = a+1;
+		System.out.println("El siguiente numero es:"+nextNum);
+
+	}
+
+}
